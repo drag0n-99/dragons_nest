4db12a4f19c8db69fd406f2133347ec1e1555e13
Kairos Tal <josh.starmer@icloud.com>
2024-06-01 17:36:40 -1000

    add routes to server, add reqwest to hit routes, add tls support

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,7 +1,59 @@
-use clap::{Arg, Command, ArgAction::Set};
+use clap::{Arg, ArgAction::Set, Command};
+use reqwest::{Client, Error};
 
+async fn list_agents_handler() -> Result<(), Error> {
+    let client = Client::builder()
+        .danger_accept_invalid_certs(true)
+        .use_rustls_tls()
+        .build()?;
 
-fn main() {
+    let content = client
+        .get("https://127.0.0.1:3031/list_agents")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("text: {content:?}");
+    Ok(())
+}
+
+async fn list_jobs_handler() -> Result<(), Error> {
+    let client = Client::builder()
+        .danger_accept_invalid_certs(true)
+        .use_rustls_tls()
+        .build()?;
+
+    let content = client
+        .get("https://127.0.0.1:3031/list_jobs")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("text: {content:?}");
+    Ok(())
+}
+
+async fn exec_cmd_handler() -> Result<(), Error> {
+    let client = Client::builder()
+        .danger_accept_invalid_certs(true)
+        .use_rustls_tls()
+        .build()?;
+
+    let content = client
+        .post("https://127.0.0.1:3031/exec_cmd")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("text: {content:?}");
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let matches = Command::new(clap::crate_name!())
         .version(clap::crate_version!())
         .about(clap::crate_description!())
@@ -29,26 +81,37 @@ fn main() {
         .arg_required_else_help(true)
         .get_matches();
 
-
-// pub fn subcommand(&self) -> Option<(&str, &ArgMatches)>
-// Returns a tuple of two values the first is the Arg id, the second is a reference to the arguments and their values
-match matches.subcommand() {
-        Some(("list-agents", _)) => println!("run agents() function"),
-        Some(("list-jobs", _)) => println!("run jobs() function"),
+    // pub fn subcommand(&self) -> Option<(&str, &ArgMatches)>
+    // Returns a tuple of two values the first is the Arg id, the second is a reference to the arguments and their values
+    match matches.subcommand() {
+        Some(("list-agents", _)) => list_agents_handler().await?,
+        // Some(("list-jobs", _)) => println!("run jobs() function"),
+        Some(("list-jobs", _)) => list_jobs_handler().await?,
         Some(("exec", sub_m)) => {
+            // Parse the agent_id to be a u128
             let agent_id: u128 = sub_m
+                // The signature for .get_one is this pub fn get_one<T>(&self, id: &str) -> Option<&T>
+                // This means that we have to specify the type that is to be returned
+                // By specify String as the type we are saying that the return type is string
+                // This makes sense because all user input from the console is a String
                 .get_one::<String>("agent-uuid")
                 .expect("Agent ID is required")
+                // Parse argument to u128
                 .parse()
                 .expect("Error: Invalid agent ID");
-            let command = sub_m.get_one::<String>("command").expect("Command is required");
+
+            let command: &String = sub_m
+                .get_one::<String>("command")
+                .expect("Command is required");
+
+            exec_cmd_handler().await?;
             println!(
                 "run exec() function, agent_id is {} and command is {}",
                 agent_id, command
             );
         }
         _ => unreachable!("Exhaustive checking in subcommand match failed"),
-
     }
 
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{Filter, Reply};
+
+fn list_agents_handler() -> impl Reply {
+    println!("IT WORKS");
+    "Listing agents"
+}
+fn list_jobs_handler() -> impl Reply {
+    println!("IT WORKS");
+    "Listing jobs"
+}
+fn exec_cmd_handler() -> impl Reply {
+    println!("IT WORKS");
+    "Executing job"
+}
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let list_agents_route = warp::path!("list_agents").map(|| list_agents_handler());
+
+    let list_jobs_route = warp::path!("list_jobs").map(|| list_jobs_handler());
+
+    let exec_cmd_route = warp::path!("exec_cmd").map(|| exec_cmd_handler());
+
+    // // Match any request and return hello world!
+    // let hello = warp::any().map(|| "Hello, World!");
+
+    let get_routes = warp::get().and(list_agents_route.or(list_jobs_route));
+    let post_routes = warp::post().and(exec_cmd_route);
+
+    let routes = get_routes.or(post_routes);
+    warp::serve(routes)
+        .tls()
+        // Specify the path to your certificate and key files.
+        .cert_path("tls/cert.pem")
+        .key_path("tls/key.rsa")
+        .run(([127, 0, 0, 1], 3031))
+        .await;
+
+    // warp::serve(hello)
+    // .run(([127, 0, 0, 1], 3030))
+    // .await;
 }
